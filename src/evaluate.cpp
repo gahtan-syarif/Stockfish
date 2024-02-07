b230c0b023b41fbfaa0a3b66e16f34bfa5e99116
gahtan-syarif <155860115+gahtan-syarif@users.noreply.github.com>
2024-02-07 21:09:48 +0700

    simplify_complexity

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -211,14 +211,12 @@ Value Eval::evaluate(const Position& pos, int optimism) {
     {
         bool smallNet = std::abs(simpleEval) > 1050;
 
-        int nnueComplexity;
-
-        Value nnue = smallNet ? NNUE::evaluate<NNUE::Small>(pos, true, &nnueComplexity)
-                              : NNUE::evaluate<NNUE::Big>(pos, true, &nnueComplexity);
+        Value nnue = smallNet ? NNUE::evaluate<NNUE::Small>(pos, true)
+                              : NNUE::evaluate<NNUE::Big>(pos, true);
 
         // Blend optimism and eval with nnue complexity and material imbalance
-        optimism += optimism * (nnueComplexity + std::abs(simpleEval - nnue)) / 512;
-        nnue -= nnue * (nnueComplexity + std::abs(simpleEval - nnue)) / 32768;
+        optimism += optimism * (std::abs(simpleEval - nnue)) / 512;
+        nnue -= nnue * (std::abs(simpleEval - nnue)) / 32768;
 
         int npm = pos.non_pawn_material() / 64;
         v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
