569d587a846a247ab1bd39618d4d4b6ef3d21af0
Gahtan Nahdi <gahtansyarif@gmail.com>
2024-02-11 04:34:53 +0700

    Revert "Update search.cpp"
    This reverts commit efe83c4476f0bfa8268967b9d5635fb4eb6666f7.

--- src/search.cpp
+++ src/search.cpp
@@ -1062,6 +1062,10 @@ moves_loop:  // When in check, search starts here
                 else if (ttValue >= beta)
                     extension = -2 - !PvNode;
 
+                // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
+                else if (cutNode)
+                    extension = -2;
+
                 // If the ttMove is assumed to fail low over the value of the reduced search (~1 Elo)
                 else if (ttValue <= value)
                     extension = -1;
