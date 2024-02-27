860151adee008e01d1851b8c95f83723b4a62f33
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-02-27 09:32:32 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1100,7 +1100,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if position is or has been on the PV (~7 Elo)
         if (ss->ttPv)
-            r -= 1 + (ttValue > alpha) + (tte->depth() >= depth);
+            r -= 2 + (ttValue > alpha);
 
         // Increase reduction for cut nodes (~4 Elo)
         if (cutNode)
