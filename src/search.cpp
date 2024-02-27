54cabe8f7028b53f1d47329373e686c4983bef51
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-02-28 05:28:20 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1104,7 +1104,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~4 Elo)
         if (cutNode)
-            r += 2;
+            r++;
 
         // Increase reduction if ttMove is a capture (~3 Elo)
         if (ttCapture)
