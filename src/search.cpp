f74ba837ef60613c607f11aac00f8083200af00a
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-03-04 16:50:29 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -814,7 +814,7 @@ Value Search::Worker::search(
         return qsearch<PV>(pos, ss, alpha, beta);
 
     // For cutNodes without a ttMove, we decrease depth by 2 if depth is high enough.
-    if (cutNode && depth >= 8 && !ttMove)
+    if (cutNode && !ttMove)
         depth -= 2;
 
     // Step 11. ProbCut (~10 Elo)
