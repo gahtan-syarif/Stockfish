88a6a218fe928d7b6a25da2b9589570da2a09592
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-03-11 07:12:34 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1043,7 +1043,7 @@ moves_loop:  // When in check, search starts here
                     // We make sure to limit the extensions in some way to avoid a search explosion
                     if (!PvNode && ss->multipleExtensions <= 16)
                     {
-                        extension = 2 + (value < singularBeta - 78 && !ttCapture);
+                        extension = 3;
                         depth += depth < 16;
                     }
                     if (PvNode && !ttCapture && ss->multipleExtensions <= 5
