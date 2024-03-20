bf40759c27d1f7c30b672210b501a12fdffb450f
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-03-21 06:13:02 +0700

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -62,7 +62,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks& networks, const Position& pos, 
                                 int npmConstant, int evalDiv, int shufflingConstant,
                                 int shufflingDiv) {
         // Blend optimism and eval with nnue complexity and material imbalance
-        optimism += optimism * (nnueComplexity + std::abs(simpleEval - nnue)) / optDiv;
+        optimism += optimism * (nnueComplexity + 533) / optDiv;
         nnue -= nnue * (nnueComplexity + std::abs(simpleEval - nnue)) / nnueDiv;
 
         int npm = pos.non_pawn_material() / 64;
