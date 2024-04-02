c0795fe1ef1d582e247fee545c09b0f95b1b91cd
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-02 17:13:44 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1083,13 +1083,6 @@ moves_loop:  // When in check, search starts here
                 else if (ttValue <= value)
                     extension = -1;
             }
-
-            // Recapture extensions (~0 Elo on STC, ~1 Elo on LTC)
-            else if (PvNode && move == ttMove && move.to_sq() == prevSq
-                     && thisThread->captureHistory[movedPiece][move.to_sq()]
-                                                  [type_of(pos.piece_on(move.to_sq()))]
-                          > 4026)
-                extension = 1;
         }
 
         // Add extension to new depth
